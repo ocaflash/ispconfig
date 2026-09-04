42b51df640a3e455dcb0290cb7e5df5494376c77
Aleksandr Ostapchuk <87189132+ocaflash@users.noreply.github.com>
2026-09-04 15:20:39 +0300

    Create ispconfig-debian13.sh

---
+++ ispconfig-debian13.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# metadata_begin
+# recipe: ISPConfig
+# tags: debian13
+# revision: 1-qs
+# description_ru: Установка ISPConfig
+# description_en: Install ISPConfig
+# metadata_end
+#
+exec > ISPConfig.log 2>&1
+echo -e 'deb http://security.debian.org trixie-security main contrib non-free non-free-firmware\n deb http://deb.debian.org/debian trixie-updates main non-free contrib non-free-firmware\n deb http://deb.debian.org/debian trixie-proposed-updates main non-free contrib non-free-firmware\n deb http://deb.debian.org/debian trixie-backports-sloppy main non-free contrib non-free-firmware\n deb http://deb.debian.org/debian trixie-backports main non-free contrib non-free-firmware\n deb http://deb.debian.org/debian trixie main non-free contrib non-free-firmware' > /etc/apt/sources.list
+echo -e 'Package: *\nPin: release a=trixie-security\nPin-Priority: 500\n\nPackage: *\nPin: release a=trixie-updates\nPin-Priority: 500\n\nPackage: *\nPin: release a=trixie-proposed-updates\nPin-Priority: 500\n\nPackage: *\nPin: release a=trixie-backports-sloppy\nPin-Priority: 500\n\nPackage: *\nPin: release a=trixie-backports\nPin-Priority: 500\n\nPackage: *\nPin: release a=trixie\nPin-Priority: 500' > /etc/apt/preferences
+DEBIAN_FRONTEND=noninteractive apt update
+DEBIAN_FRONTEND=noninteractive apt -y upgrade
+DEBIAN_FRONTEND=noninteractive apt -y dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt -y install --no-install-recommends cron postfix-mysql postfix-doc mariadb-client mariadb-server getmail6 rkhunter binutils dovecot-imapd dovecot-pop3d dovecot-mysql dovecot-sieve dovecot-lmtpd sudo curl rsyslog gnupg2 lsb-release rspamd redis clamav clamav-daemon unzip arj nomarch lzop cabextract p7zip p7zip-full unrar lrzip libnet-ldap-perl libauthen-sasl-perl clamav-docs daemon libnet-ident-perl zip libdbd-mysql-perl postgrey php8.4 php8.4-fpm php8.4-common php8.4-gd php8.4-mysql php8.4-cli php8.4-cgi php-pear mcrypt imagemagick libruby php8.4-curl php8.4-intl php8.4-pspell php8.4-sqlite3 php8.4-tidy php8.4-xmlrpc php8.4-xsl php-imagick php8.4-zip php8.4-mbstring php8.4-soap php8.4-opcache php-apcu php8.4-mcrypt pure-ftpd-common pure-ftpd-mysql fail2ban iptables nginx-light fcgiwrap certbot patch pwgen quota wget dnsmasq jailkit
+echo -e "PrivateMirror https://clamav-mirror.ru/\nPrivateMirror https://mirror.truenetwork.ru/clamav/\nPrivateMirror http://mirror.truenetwork.ru/clamav/\nScriptedUpdates no" >> /etc/clamav/freshclam.conf
+sed -i 's/#submission inet n       -       y       -       -       smtpd/submission inet n - y - - smtpd/' /etc/postfix/master.cf
+sed -i 's/#  -o syslog_name=postfix\/submission/ -o syslog_name=postfix\/submission/' /etc/postfix/master.cf
+sed -i 's/#  -o smtpd_tls_security_level=encrypt/ -o smtpd_tls_security_level=encrypt/' /etc/postfix/master.cf
+sed -i 's/#  -o smtpd_sasl_auth_enable=yes/ -o smtpd_sasl_auth_enable=yes/' /etc/postfix/master.cf
+sed -i 's/#submissions     inet  n       -       y       -       -       smtpd/submissions inet n - y - - smtpd/' /etc/postfix/master.cf
+sed -i 's/#  -o smtpd_tls_wrappermode=yes/ -o smtpd_tls_wrappermode=yes/' /etc/postfix/master.cf
+echo 'smtputf8_enable=no' >> /etc/postfix/main.cf
+systemctl restart postfix
+echo -e '[mysqld]\nsql_mode=\nsync_binlog=0\ntransaction-isolation=READ-COMMITTED\nquery_cache_type=ON\ninnodb_flush_log_at_trx_commit=0\ninnodb_flush_method=O_DIRECT\ninnodb_buffer_pool_instances=1\ninnodb_strict_mode=OFF\nlow-priority-updates\nslow_query_log\nlong_query_time=29.99\nslow_query_log_file=/var/log/mysql/mariadb-slow.log' > /etc/mysql/mariadb.conf.d/51-server.cnf
+mkdir -p /var/log/mysql
+chown mysql:mysql /var/log/mysql
+systemctl restart mariadb
+echo 'servers = "127.0.0.1";' > /etc/rspamd/local.d/redis.conf
+echo "nrows = 2500;" > /etc/rspamd/local.d/history_redis.conf 
+echo "compress = true;" >> /etc/rspamd/local.d/history_redis.conf
+echo "subject_privacy = true;" >> /etc/rspamd/local.d/history_redis.conf
+systemctl restart rspamd
+sed -i 's/VIRTUALCHROOT=false/VIRTUALCHROOT=true/' /etc/default/pure-ftpd-common
+echo 1 > /etc/pure-ftpd/conf/TLS
+openssl req -x509 -nodes -days 7300 -newkey rsa:2048 -keyout /etc/ssl/private/pure-ftpd.pem -out /etc/ssl/private/pure-ftpd.pem  -subj "/C=RU/ST=Moscow/L=Moscow/O=/OU=/CN=$(hostname -f)/emailAddress=root@$(hostname -f)"
+chmod 600 /etc/ssl/private/pure-ftpd.pem
+systemctl restart pure-ftpd-mysql
+echo -e '[pure-ftpd]\nenabled = true\nport = ftp\nfilter = pure-ftpd\nlogpath = /var/log/syslog\nmaxretry = 3\n\n[dovecot]\nenabled = true\nfilter = dovecot\nlogpath = /var/log/mail.log\nmaxretry = 5\n\n[postfix-sasl]\nenabled = true\nport = smtp\nfilter = postfix[mode=auth]\nlogpath = /var/log/mail.log\nmaxretry = 3' > /etc/fail2ban/jail.local
+systemctl restart fail2ban
+echo -e '\#!/bin/bash\n/usr/sbin/nginx -t && /usr/sbin/nginx -s reload' > /usr/local/bin/reload.sh
+chmod +x /usr/local/bin/reload.sh
+echo '37 16 */29 * * /usr/local/bin/reload.sh' | crontab
+export passwd_root=$(pwgen 16 1)
+mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('${passwd_root}');FLUSH PRIVILEGES;"
+sed -i "s/^user.*$/&\npassword = \"$passwd_root\"/"  /etc/mysql/debian.cnf
+cd /tmp
+wget http://www.ispconfig.org/downloads/ISPConfig-3-stable.tar.gz
+tar xfz ISPConfig-3-stable.tar.gz
+cd ispconfig3_install/install/
+export passwd_isp=$(pwgen 16 1)
+export passwd_db=$(pwgen 16 1)
+echo -e "[install]\nlanguage=en\ninstall_mode=standard\nhostname=$(hostname -f)\nmysql_hostname=localhost\nmysql_port=3306\nmysql_root_user=root\nmysql_root_password=$passwd_root\nmysql_database=dbispconfig\nmysql_charset=utf8\nhttp_server=nginx\nispconfig_port=81\nispconfig_use_ssl=n\nispconfig_admin_password=$passwd_isp\n\n[ssl_cert]\nssl_cert_country=RU\nssl_cert_state=Moscow\nssl_cert_locality=Moscow\nssl_cert_organisation=\nssl_cert_organisation_unit=\nssl_cert_common_name=$(hostname -f)\nssl_cert_email=hostmaster@$(hostname -f)\n\n[expert]\nmysql_ispconfig_user=ispconfig\nmysql_ispconfig_password=$passwd_db\njoin_multiserver_setup=n\nmysql_master_hostname=localhost\nmysql_master_root_user=root\nmysql_master_root_password=''\nmysql_master_database=dbispconfig\nconfigure_mail=y\nconfigure_jailkit=y\nconfigure_ftp=y\nconfigure_dns=n\nconfigure_apache=n\nconfigure_web=y\nconfigure_nginx=y\nconfigure_firewall=y\ninstall_ispconfig_web_interface=y" > autoinstall.ini
+php install.php --autoinstall=autoinstall.ini
+echo -e "admin $passwd_isp\n$passwd_root" > ~/.ispconfig.ini
+echo -e 'User-Agent: *\nDisallow: /' > /var/www/robots.txt
+mkdir /etc/systemd/system/openvpn-client@.service.d
+echo -e '[Service]\nRestart=always\nRestartSec=10000ms\nStartLimitInterval=0' > /etc/systemd/system/openvpn-client@.service.d/autorestart.conf
